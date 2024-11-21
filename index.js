4b29ef0533e32fc8a1f8bb3104915f357a6af736
Argel Alcantara <argelsteam1@gmail.com>
2024-11-21 17:53:30 +0100

    Creazione Modal

---
+++ index.js
@@ -0,0 +1,33 @@
+/*** Esercizio 1 ***/
+// var nome = prompt('Inserisci il tuo nome')
+// console.log('Ciao '+ nome+'!');
+
+
+/*** Esercizio 2 ***/
+// var x =  5;
+// var y = 10;
+// var z = x;
+// x = y;
+// y = z;
+// console.log(x);
+// console.log(y);
+
+
+/*** Esercizio 3 ***/
+var eta = prompt('Quanti anni hai?');
+var puoEntrareoNo = eta >= 14;
+console.log('Hai '+ eta +' anni');
+console.log('Il cliente è ammesso in sala? '+puoEntrareoNo);
+
+
+/*** Esercizio 4 ***/
+// var peso = prompt('Inserisci il tuo peso(kg):')
+// var altezza = prompt('Inserisci la tua altezza (m):')
+// var bmi = peso / (altezza*altezza);
+// var sovrappeso = bmi > 25;
+// console.log('Peso: '+peso);
+// console.log('Altezza: '+altezza);
+// console.log('Il tuo BMI: '+bmi);5
+// console.log('Sei sovrappeso? '+sovrappeso);
+
+
